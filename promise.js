3ccb3ab33cabe55553e6f57ae37e43ccc0d2c3a4
shadyaahh <tshadiya4@gmail.com>
2025-09-08 09:16:35 +0530

    committed

---
+++ promise.js
@@ -0,0 +1,8 @@
+//const promise = new PromiseRejectionEvent((resolve,reject)=>{
+//asynchronous operactioin
+//})
+
+//resolve called when the function is successful
+//reject called when the function is fallure
+//.then  = excute the promise is resolve 
+//.catch = execute oif the promis is resolved or rejected
